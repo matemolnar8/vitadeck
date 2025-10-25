1774a5912c5fdc356d06f993edcdad93d7f10fef
matemolnar8 <doglotteger@gmail.com>
2025-10-25 09:14:56 +0200

    Add react compiler and babel

--- js/rollup.config.js
+++ js/rollup.config.js
@@ -3,7 +3,14 @@ import typescript from "@rollup/plugin-typescript";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from "@rollup/plugin-replace";
+import babel from "@rollup/plugin-babel";
 import { corejsPlugin } from "rollup-plugin-corejs";
+
+/*
+
+TypeScript ES2015 -> Babel Es
+
+*/
 
 export default defineConfig({
   input: {
@@ -20,12 +27,35 @@ export default defineConfig({
   },
   plugins: [
     typescript(),
+    commonjs(),
     nodeResolve(),
+    babel({
+      babelHelpers: "bundled",
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+      plugins: [
+        [
+          "babel-plugin-react-compiler",
+          {
+            target: "18",
+          },
+        ],
+      ],
+      exclude: [/node_modules\/core-js/],
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: { ie: "10" },
+            bugfixes: true,
+            modules: false,
+          },
+        ],
+      ],
+    }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify('development'),
+      "process.env.NODE_ENV": JSON.stringify("development"),
       preventAssignment: true,
     }),
-    commonjs(),
     corejsPlugin(),
   ],
 });
